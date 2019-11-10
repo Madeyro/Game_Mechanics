d5f1e47c3c6e3ca4c8a3c6073c75396bae3e14eb
Frutnt <mistrtomato@gmail.com>
2019-11-10 21:10:55 +0100

    Added level 01 by UNITY

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,59 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    public float speed = 6f;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidBody;
+    int floorMask;
+    float camRayLength = 100f;
+
+    private void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal"); // -1, 0 or 1
+        float vertical = Input.GetAxisRaw("Vertical"); // -1, 0 or 1
+
+        Move(horizontal, vertical);
+        Turn();
+        Animation(horizontal, vertical);
+    }
+
+    private void Move(float horizontal, float vertical)
+    {
+        movement.Set(horizontal, 0f, vertical);
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidBody.MovePosition(transform.position + movement);
+    }
+
+    private void Turn()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidBody.MoveRotation(newRotation);
+        }
+    }
+
+    private void Animation(float horizontal, float vertical)
+    {
+        bool walking = horizontal != 0f || vertical != 0f;
+        anim.SetBool("IsWalking", walking);
+    }
 }
