bdd05c477e04b5ef2b6177006600b54ea719fba6
Madeyro <maroskopec@outlook.com>
2019-11-16 15:59:27 +0100

    Medkit functional

---
+++ Assets/_Complete-Game/Scripts/Managers/MedkitManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace CompleteProject
+{
+    public class MedkitManager : MonoBehaviour
+    {
+
+        public PlayerHealth playerHealth;       // Reference to the player's heatlh.
+        public GameObject medkit;               // The enemy prefab to be spawned.
+        public float spawnTime = 30f;           // How long between each spawn.
+        public Transform spawnPoint;            // A spawn point.
+
+
+        void Start()
+        {
+            // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+            InvokeRepeating("Spawn", spawnTime, spawnTime);
+        }
+
+
+        void Spawn()
+        {
+            // If the player has no health left...
+            if (playerHealth.currentHealth <= 0f)
+            {
+                // ... exit the function.
+                return;
+            }
+
+            if (!GameObject.Find("Medkit(Clone)"))
+            {
+                Instantiate(medkit, spawnPoint);
+            }
+        }
+    }
+}
---
+++ Assets/_Complete-Game/Scripts/Medkit/HealOnTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+namespace CompleteProject
+{
+    public class HealOnTrigger : MonoBehaviour
+    {
+        public float restoreAmount = 0.3f;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                PlayerHealth hp = other.gameObject.GetComponent<PlayerHealth>();
+                hp.Heal(restoreAmount);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
